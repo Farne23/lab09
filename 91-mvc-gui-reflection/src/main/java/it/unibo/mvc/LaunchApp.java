cd3fb2a3a736af347926fc15f41fc49709aa3897
Michele Farneti <Michele.Farneti@studio.unibo.it>
2023-11-20 12:51:08 +0100

    Solved exercise 91, created a new output only view and implemented support for multiple views, now new views are also loaded by reflection

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,8 +1,13 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.InvocationTargetException;
+
+import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
+import it.unibo.mvc.view.DrawNumberStdoutView;
 import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
@@ -23,9 +28,20 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
+
+        var viewIO = Class.forName("it.unibo.mvc.view.DrawNumberSwingView");
+        var viewO = Class.forName("it.unibo.mvc.view.DrawNumberStdoutView");
+
+        for(int i = 0; i <3 ; i++){
+            if(DrawNumberView.class.isAssignableFrom(viewIO)){
+                app.addView((DrawNumberView)viewIO.getConstructor().newInstance());
+            }
+            if(DrawNumberView.class.isAssignableFrom(viewO)){
+                app.addView((DrawNumberView)viewO.getConstructor().newInstance());
+            }
+        }
     }
 }
